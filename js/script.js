986bb0d2996eb5884132aeb9197801f8a24959f9
svetlana <zhukovaou@gmail.com>
2015-03-05 20:56:54 +0300

    картинки крестиков

--- js/script.js
+++ js/script.js
@@ -7,13 +7,30 @@ $(document).ready(function() {
 
     createBoard(elementsNum);
 
+    function player () {
+        var
+            playerType = $('.player').val();
+        if (playerType == '1') {
+            return true;
+        }
+        return false;
+    }
+
     function createBoard (elements){
         var
             elementDiv = '',
-            i = 0;
-
+            i = 0,
+            playerClass = 'xo_x';
+        if (player()) {
+            playerClass = 'xo_o';
+        }
         for (i; i<elements * elements; i++) {
-            elementDiv += '<div id="'+i+'" class="board_element"></div>';
+            if (i & 1) {
+                playerClass = 'xo_x';
+            } else {
+                playerClass = 'xo_o';
+            }
+            elementDiv += '<div id="'+i+'" class="board_element"><div class="xo ' + playerClass + '"></div></div>';
         }
         $('.xo_board').html(elementDiv);
     }
